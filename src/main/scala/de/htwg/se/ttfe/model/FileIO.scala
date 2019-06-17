5658a39039ac1e9dc9cda59fe341c618202e2b64
nistephan <nicolai.stephan@gmx.net>
2019-06-17 13:04:26 +0200

    added try monad

--- src/main/scala/de/htwg/se/ttfe/model/FileIO.scala
+++ src/main/scala/de/htwg/se/ttfe/model/FileIO.scala
@@ -3,6 +3,7 @@ package de.htwg.se.ttfe.model
 import java.io.{File, PrintWriter}
 
 import com.google.inject.Guice
+import play.api.libs.json._
 import de.htwg.se.ttfe.TTFEModule
 import de.htwg.se.ttfe.model.fieldComponent.FieldInterface
 import de.htwg.se.ttfe.model.fieldComponent.fieldBaseImpl.{Field, Matrix}
@@ -11,13 +12,16 @@ import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 class FileIO extends FileIOInterface {
-  def load: Try[FieldInterface] = {
-    val injector = Guice.createInjector(new TTFEModule)
 
-    val source: String = Source.fromFile("src/main/resources/grid.json").getLines.mkString
-    val json: JsValue = Json.parse(source)
-    json match {
+  def readFile(filename: String): Try[String] = {
+    Try(Source.fromFile(filename).getLines.mkString)
+  }
+
+  def load: FieldInterface = {
+    //val injector = Guice.createInjector(new TTFEModule)
+    readFile("src/main/resources/grid.json") match {
       case Success(v) =>
+        val json: JsValue = Json.parse(v)
         val size = (json \ "size").get.toString.toInt
         def loop(index: Int, matrix: Matrix[Integer]): Matrix[Integer] ={
           if(index != size * size){
@@ -30,12 +34,11 @@ class FileIO extends FileIOInterface {
           }
         }
         val matrix = loop(0, new Matrix[Integer](size, 0))
-        Success(new Field(matrix))
-      case Failure(e) =>
-        println("Error: " + e.getMessage)
-        Failure(e)
+        new Field(matrix)
+      case Failure(f) =>
+        println("Error: " + f.getMessage)
+        new Field(4)
     }
-
   }
 
   def save(field: FieldInterface): Unit ={
